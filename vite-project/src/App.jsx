949891053f5448ac6eb0dc8444847bed4f032305
abuhasan-Ohovro <abuhasanmondal24@gmail.com>
2024-03-15 12:41:13 +0600

    React project set up

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import First from "./first";
+
+
+
+function App() {
+
+  return (
+    
+
+      
+<First/>
+  
+  );
+}
+
+export default App
---
+++ vite-project/src/first.jsx
@@ -0,0 +1,9 @@
+import App from "./App"
+
+function First(){
+    return(
+        <h3>React Application</h3>
+    )
+}
+
+export default First
